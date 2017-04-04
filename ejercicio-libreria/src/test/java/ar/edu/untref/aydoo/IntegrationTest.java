6757abfb9b1804b79ecbd80531547cf747389750
dehernan <moreno.hernan12@gmail.com>
2017-04-04 10:41:09 -0300

    segundo test ejercicio libreria, con dos items adquiridos en el mes

--- ejercicio-libreria/src/test/java/ar/edu/untref/aydoo/IntegrationTest.java
+++ ejercicio-libreria/src/test/java/ar/edu/untref/aydoo/IntegrationTest.java
@@ -1,5 +1,6 @@
 package ar.edu.untref.aydoo;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
@@ -7,12 +8,31 @@ import static ar.edu.untref.aydoo.Month.ENERO;
 
 public class IntegrationTest{
 
+    ChargeCalculator calculator;
+
+    @Before
+    public void createCalculator(){
+
+        calculator = new ChargeCalculator();
+
+    }
     @Test
-    public void createCustomerTest(){
-        ChargeCalculator calculator = new ChargeCalculator();
+    public void shouldCalculateAmountOfOneItem(){
         Book firstBook = new Book("El psicoanalista", 30.0);
         Customer horacio = new Customer("Horacio", "Remedios de Escalada 1234", "San Martin");
         Purchase horacioFirstPurchase = new Purchase(horacio, firstBook, ENERO);
         Assert.assertEquals(30.0, calculator.calculateAmountToBeCharged(ENERO, horacio),0.0);
     }
+
+    @Test
+    public void shouldCalculateAmountOfTwoItems(){
+        Book firstBook = new Book("El psicoanalista", 30.0);
+        Book secondBook = new Book("Martin Fierro", 50.5);
+        Customer horacio = new Customer("Horacio", "Remedios de Escalada 1234", "San Martin");
+        Purchase horacioFirstPurchase = new Purchase(horacio, firstBook, ENERO);
+        Purchase horacioSecondPurchase = new Purchase(horacio, secondBook, ENERO);
+        Assert.assertEquals(80.5, calculator.calculateAmountToBeCharged(ENERO, horacio),0.0);
+    }
+
+
 }
