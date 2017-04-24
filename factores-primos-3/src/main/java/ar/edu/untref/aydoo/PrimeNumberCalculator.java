263d3200bdb68e9dab7935c362d93985d6209643
dehernan <moreno.hernan12@gmail.com>
2017-04-24 12:13:35 -0300

    Se agrega el ejercicio factores-primos-3, con las nuevas funcionalidades de sort y output-file

---
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/PrimeNumberCalculator.java
@@ -0,0 +1,121 @@
+package ar.edu.untref.aydoo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PrimeNumberCalculator{
+
+    public static List<Integer> factors;
+    public static List<String> flagParameters;
+    public static String output;
+    public static String format;
+    public static String order;
+    public static String path;
+    public static boolean writeOutputFile;
+
+    public static final void main(String arg[]){
+        factors = new ArrayList<>();
+        flagParameters = new ArrayList<>();
+        output = "";
+        format = "pretty";
+        order = "asc";
+        path = "";
+        writeOutputFile = false;
+
+        int number = Integer.parseInt(arg[0]);
+        for(int i = 0; i < arg.length; i++){
+            arg[i]=arg[i].toLowerCase();
+        }
+
+        output ="Factores primos "+number+": ";
+        calculateFactors(number);
+        evaluateParameters(arg);
+        orderFactors(order);
+        formatOutput(format);
+        if(writeOutputFile) {
+            printFile(path);
+        }
+
+    }
+
+    private static void evaluateParameters(String[] arg) {
+        for(String i: arg) {
+            if (i.startsWith("--format=")) {
+                format = i.substring(9, i.length());
+            }
+            if (i.startsWith("--sort=")) {
+                order = i.substring(7, i.length());
+            }
+            if (i.startsWith("--output-file=")) {
+                path = i.substring(14, i.length());
+                writeOutputFile = true;
+            }
+        }
+    }
+
+    public static void calculateFactors(int number){
+        if(number == 1){
+            factors.add(number);
+        }
+        for(int i = 2; i<=number; i++){
+            while(number%i==0) {
+                number = number/i;
+                factors.add(i);
+            }
+        }
+    }
+
+    public static void formatOutput(String format){
+        switch(format){
+            case "":
+            case "pretty":
+                for(int i = 0; i < factors.size(); i++){
+                    output = output.concat(factors.get(i)+" ");
+                }
+                break;
+            case "quiet":
+                output = output.concat("\n");
+                for(int i = 0; i<factors.size(); i++){
+                    output = output.concat(factors.get(i)+"\n");
+                }
+                break;
+            default:
+                output = "Formato no aceptado. Las opciones posibles son: pretty o quiet.";
+        }
+    }
+
+    public static void orderFactors(String order){
+        switch(order){
+            case "":
+            case "asc":
+                Collections.sort(factors);
+                break;
+            case "desc":
+                Collections.sort(factors, Collections.reverseOrder());
+                break;
+            default:
+                output = "Orden no aceptado. Las opciones posibles son: asc o desc.";
+        }
+    }
+
+    public static void printFile(String path){
+        if(path.endsWith(".txt")) {
+            String printed = output;
+            try {
+                PrintWriter writer = new PrintWriter(path, "UTF-8");
+                System.out.println(printed);
+                writer.println(printed);
+                writer.close();
+            } catch (IOException e) {
+                e.getMessage();
+            }
+        }
+        else{
+            output = "El archivo debe ser especificado con formato .txt";
+        }
+    }
+
+}
---
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/PrimeNumberCalculatorTest.java
@@ -0,0 +1,165 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class PrimeNumberCalculatorTest{
+
+    PrimeNumberCalculator calculator;
+
+    @Before
+    public void createCalculator(){
+
+        calculator = new PrimeNumberCalculator();
+
+    }
+
+    @Test
+    public void number360WithoutFormatTest() {
+
+        String[] args = new String[2];
+        args[0] = "360";
+        args[1] = "";
+
+        calculator.main(args);
+
+        Assert.assertEquals("Factores primos 360: 2 2 2 3 3 5 ", calculator.output);
+    }
+
+    @Test
+    public void number360WithQuietFormatTest() {
+
+        String[] args = new String[2];
+        args[0] = "360";
+        args[1] = "--format=quIet";
+
+        calculator.main(args);
+
+        Assert.assertEquals("Factores primos 360: \n2\n2\n2\n3\n3\n5\n", calculator.output);
+    }
+
+    @Test
+    public void number360WithQuietFormatAndAscOrderTest() {
+
+        String[] args = new String[3];
+        args[0] = "360";
+        args[1] = "--format=quIet";
+        args[2] = "--sort=asc";
+
+        calculator.main(args);
+
+        Assert.assertEquals("Factores primos 360: \n2\n2\n2\n3\n3\n5\n", calculator.output);
+    }
+
+    @Test
+    public void number360WithQuietFormatAndAscOrderAndOutputFileTest() {
+
+        PrimeNumberCalculator otra = new PrimeNumberCalculator();
+        String[] args = new String[4];
+        args[0] = "360";
+        args[1] = "--format=quIet";
+        args[2] = "--sort=asc";
+        args[3] = "--output-file=number360WithQuietFormatAndAscOrderAndOutputFileTest.txt";
+
+        otra.main(args);
+
+        Assert.assertEquals("Factores primos 360: \n2\n2\n2\n3\n3\n5\n", otra.output);
+    }
+
+    @Test
+    public void number360WithQuietFormatAndDescOrderAndOutputFileTest() {
+
+        PrimeNumberCalculator otra = new PrimeNumberCalculator();
+        String[] args = new String[4];
+        args[0] = "360";
+        args[1] = "--format=quIet";
+        args[2] = "--sort=desc";
+        args[3] = "--output-file=number360WithQuietFormatAndDescOrderAndOutputFileTest.txt";
+
+        otra.main(args);
+
+        Assert.assertEquals("Factores primos 360: \n5\n3\n3\n2\n2\n2\n", otra.output);
+    }
+
+    @Test
+    public void number360WithPrettyFormatTest() {
+
+        String[] args = new String[2];
+        args[0] = "360";
+        args[1] = "--format=pretty";
+
+        calculator.main(args);
+
+        Assert.assertEquals("Factores primos 360: 2 2 2 3 3 5 ", calculator.output);
+    }
+
+    @Test
+    public void number360WithPrettyFormatAndDescOrderTest() {
+
+        String[] args = new String[3];
+        args[0] = "360";
+        args[1] = "--format=pretty";
+        args[2] = "--sort=desc";
+
+        calculator.main(args);
+
+        Assert.assertEquals("Factores primos 360: 5 3 3 2 2 2 ", calculator.output);
+    }
+
+    @Test
+    public void number360WithPrettyFormatAndDescOrderAndOutputFileTest() {
+
+        String[] args = new String[4];
+        args[0] = "360";
+        args[1] = "--format=pretty";
+        args[2] = "--sort=desc";
+        args[3] = "--output-file=number360WithPrettyFormatAndDescOrderAndOutputFileTest.txt";
+
+        calculator.main(args);
+
+        Assert.assertEquals("Factores primos 360: 5 3 3 2 2 2 ", calculator.output);
+    }
+
+    @Test
+    public void number360WithWrongFormatParameterTest() {
+
+        String[] args = new String[2];
+        args[0] = "360";
+        args[1] = "--format=yerba";
+
+        calculator.main(args);
+
+        Assert.assertEquals("Formato no aceptado. Las opciones posibles son: pretty o quiet.", calculator.output);
+    }
+
+    @Test
+    public void number360WithWrongFormatParameterShouldReturnPrettyFormat() {
+
+        String[] args = new String[2];
+        args[0] = "360";
+        args[1] = "-format=quiet";
+
+        calculator.main(args);
+
+        Assert.assertEquals("Factores primos 360: 2 2 2 3 3 5 ", calculator.output);
+    }
+
+    @Test
+    public void number360WithWrongOutputFileParameterTest() {
+
+        String[] args = new String[3];
+        args[0] = "360";
+        args[1] = "--format=quiet";
+        args[2] = "--output-file=number360WithWrongOutputFileParameterTest.tx";
+
+        calculator.main(args);
+
+        Assert.assertEquals("El archivo debe ser especificado con formato .txt", calculator.output);
+    }
+
+
+
+
+}
